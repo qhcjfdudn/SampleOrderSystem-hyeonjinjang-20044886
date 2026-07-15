19df04f2a9dcfb079835a0d78b588c3f551e34a1
qhcjfdudn <jhjhyjin@naver.com>
2026-07-15 13:52:39 +0900

    3단계: 시료 등록(SampleController.register) 구현 (TDD)
    신규 시료를 재고 0으로 생성해 SampleRepository에 저장하는
    SampleController.register를 추가. 조회/검색/View 연동은 다음 사이클.

---
+++ app/controllers/sample_controller.py
@@ -0,0 +1,17 @@
+from app.models.sample import Sample
+
+
+class SampleController:
+    def __init__(self, sample_repository):
+        self.sample_repository = sample_repository
+
+    def register(self, id, name, avg_production_time, yield_rate):
+        sample = Sample(
+            id=id,
+            name=name,
+            avg_production_time=avg_production_time,
+            yield_rate=yield_rate,
+            stock=0,
+        )
+        self.sample_repository.save(sample)
+        return sample
