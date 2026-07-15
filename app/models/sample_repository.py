c1986b565ff0f64c668c7112f7596c011dfd0d08
qhcjfdudn <jhjhyjin@naver.com>
2026-07-15 13:41:45 +0900

    2단계: SampleRepository 구현 (TDD)
    data_persistence.DataPersistence를 통해 Sample을 저장/조회하는
    SampleRepository를 추가. save는 Sample을 dict로 변환해 create를
    호출하고, find_by_id는 read 결과를 Sample로 복원한다.

---
+++ app/models/sample_repository.py
@@ -0,0 +1,24 @@
+from data_persistence import DataPersistence
+
+from app.models.sample import Sample
+
+
+class SampleRepository:
+    def __init__(self, file_path):
+        self._data_persistence = DataPersistence(file_path)
+
+    def save(self, sample: Sample) -> None:
+        record = {
+            "id": sample.id,
+            "name": sample.name,
+            "avg_production_time": sample.avg_production_time,
+            "yield_rate": sample.yield_rate,
+            "stock": sample.stock,
+        }
+        self._data_persistence.create(record)
+
+    def find_by_id(self, id):
+        record = self._data_persistence.read(id)
+        if record is None:
+            return None
+        return Sample(**record)
